d37c150f3fd2d67e51d626664712037357833a24
wandi1209 <wandider@gmail.com>
2026-03-03 03:06:54 +0700

    feat(gamification): add QuestContext, revamp Dashboard with XP and achievements, auto-track tasks and notes

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Notes from './pages/Notes';
 import Tasks from './pages/Tasks';
 import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import { QuestProvider } from './context/QuestContext';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/notes" element={<Notes />} />
-        <Route path="/tasks" element={<Tasks />} />
-      </Routes>
-    </BrowserRouter>
+    <QuestProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/notes" element={<Notes />} />
+          <Route path="/tasks" element={<Tasks />} />
+        </Routes>
+      </BrowserRouter>
+    </QuestProvider>
   );
 }
 
